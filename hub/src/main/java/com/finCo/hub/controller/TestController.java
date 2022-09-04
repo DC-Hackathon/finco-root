2024b380319f437e2788e7a4cd29a5872ecee9c7
delta12kilo <dk9044552577@gmail.com>
2022-09-04 17:33:46 +0530

    test commit

--- hub/src/main/java/com/finCo/hub/controller/TestController.java
+++ hub/src/main/java/com/finCo/hub/controller/TestController.java
@@ -6,15 +6,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
+import java.util.function.Function;
+
 @RestController
 @Api(value = "Api test swagger.")
 @RequestMapping("/api")
 public class TestController {
 
 
-    @GetMapping("/getTest")
-    public String getFromFlask(){
-        return " sdadadadad";
+    @GetMapping("/getTest/{id}")
+    public String getOddEven(@PathParam("id")Integer id){
+
+        Function<Integer, String> evnOrOdd = x -> x%2==0?"even":"odd";
+        return " sdadadadad => "+evnOrOdd.apply(id);
     }
 
     @PostMapping("/getTest")
